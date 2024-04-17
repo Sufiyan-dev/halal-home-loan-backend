301a97b882971676e1b262a40e3e3fcf9c2d7a92
Sufiyan <sufiyanmemon687@gmail.com>
2024-04-17 16:47:17 -0400

    changed from get to post

--- src/api/routes/erc1155.route.js
+++ src/api/routes/erc1155.route.js
@@ -2,10 +2,10 @@ const express = require('express');
 const { balanceController, balanceBatchController, uriController, isApproveForAllController, setApprovalForAllController } = require('../controllers/erc1155.controller');
 const router = express.Router();
 
-router.get("/balance",balanceController);
-router.get("/balanceBatch",balanceBatchController);
-router.get("/uri",uriController);
-router.get("/isApproveForAll",isApproveForAllController);
+router.post("/balance",balanceController);
+router.post("/balanceBatch",balanceBatchController);
+router.post("/uri",uriController);
+router.post("/isApproveForAll",isApproveForAllController);
 router.post("/setApproveFotAll",setApprovalForAllController);
 
 module.exports = router;
--- src/api/routes/erc20.route.js
+++ src/api/routes/erc20.route.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { allowanceController, approveController, balanceController, symbolController } = require('../controllers/erc20.controller.js');
 const router = express.Router();
 
-router.get('/balance',balanceController);
-router.get('/allowance',allowanceController);
-router.get('/symbol',symbolController);
+router.post('/balance',balanceController);
+router.post('/allowance',allowanceController);
+router.post('/symbol',symbolController);
 router.post('/approve',approveController);
 
 module.exports = router;
